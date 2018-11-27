94b4834467a0072a979e02f5e09b2d194bcab419
Vincent Van Driessche <vincent@getsilverfin.com>
2018-11-27 15:03:00 +0100

    Adds disconnect after connecting

--- lib/health_check/redis_health_check.rb
+++ lib/health_check/redis_health_check.rb
@@ -2,19 +2,18 @@ module HealthCheck
   class RedisHealthCheck
     extend BaseHealthCheck
 
-    def initialize()
+    def self.check
       unless defined?(::Redis)
         raise "Wrong configuration. Missing 'redis' gem"
       end
 
-      @redis_client ||= ::Redis.new(url: HealthCheck.redis_url)
-    end
-
-    def self.check
-      res = @redis_client.ping
+      redis_client ||= ::Redis.new(url: HealthCheck.redis_url)
+      res = redis_client.ping
       res == 'PONG' ? '' : "Redis.ping returned #{res.inspect} instead of PONG"
     rescue Exception => e
       create_error 'redis', e.message
+    ensure
+      redis_client.disconnect!
     end
   end
 end
--- lib/health_check/resque_health_check.rb
+++ lib/health_check/resque_health_check.rb
@@ -2,19 +2,18 @@ module HealthCheck
    class ResqueHealthCheck
      extend BaseHealthCheck
 
-    def initialize()
+     def self.check
       unless defined?(::Redis)
         raise "Wrong configuration. Missing 'redis' gem"
       end
 
-      @redis_client ||= ::Redis.new(url: HealthCheck.redis_url)
-    end
-
-     def self.check
-      res = @redis_client.ping
+      redis_client ||= ::Redis.new(url: HealthCheck.redis_url)
+      res = redis_client.ping
       res == 'PONG' ? '' : "Resque.redis.ping returned #{res.inspect} instead of PONG"
      rescue Exception => e
        create_error 'resque-redis', e.message
+    ensure
+      redis_client.disconnect!
      end
    end
 end
