0e67240a4ec862ec8f988347ba2a6630fb966ed9
Vincent Van Driessche <vincent@getsilverfin.com>
2018-11-27 11:16:16 +0100

    Adds shared redis connection per instance

--- lib/health_check/resque_health_check.rb
+++ lib/health_check/resque_health_check.rb
@@ -2,11 +2,16 @@ module HealthCheck
    class ResqueHealthCheck
      extend BaseHealthCheck
 
+    def initialize()
+      unless defined?(::Redis)
+        raise "Wrong configuration. Missing 'redis' gem"
+      end
+
+      @redis_client ||= ::Redis.new(url: HealthCheck.redis_url)
+    end
+
      def self.check
-       unless defined?(::Resque)
-         raise "Wrong configuration. Missing 'resque' gem"
-       end
-      res = ::Resque.redis.ping
+      res = @redis_client.ping
       res == 'PONG' ? '' : "Resque.redis.ping returned #{res.inspect} instead of PONG"
      rescue Exception => e
        create_error 'resque-redis', e.message
